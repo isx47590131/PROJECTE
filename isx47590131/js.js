0141a913ac98959ef944321f576307e7db9942ec
isx47590131 <arnauesteban@hotmail.com>
2018-03-26 21:12:01 +0200

    html, css, java

---
+++ isx47590131/js.js
@@ -0,0 +1,11 @@
+
+function cambiar(){
+	var caja1 = document.getElementById("izq");
+	caja1.style.display="none";
+	var caja2 = document.getElementById("izq2");
+	caja2.style.display="block";
+
+
+
+
+}
